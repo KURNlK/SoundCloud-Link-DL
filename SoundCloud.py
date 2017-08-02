e7706b6772322891a1873c0d5fbe2f40e59296a2
KURNlK <30648284+KURNlK@users.noreply.github.com>
2017-08-02 00:07:46 -0700

    Add files via upload

--- SoundCloud.py
+++ SoundCloud.py
@@ -1,32 +1,36 @@
-import webdriver
 import requests
 import bs4
-import print
 import datetime
 import time
 
-print('=============Hello=============')
-print('=========WELCOME TO SoundCloudDownload v1==========')
+print('==============Hello==============')
+print('====WELCOME TO SoundCloudDownload v1====')
 print('=======================================')
-print('===produced by @KURNlK no copy===')
+print('===Produced by @KURNlK no copy===')
 
+time.sleep(2)
+print "Where xxxxxxxxx = link located on your Chrome browser"
 
 time.sleep(2)
 
-print "site = soundcloud.com"
+print "Site = https://www.soundcloud.com "
 time.sleep(2)
-print "soundcloud.com/xxxxxxxxxx, is this correct brother?"
+print "https://www.soundcloud.com/xxxxxxxxx , is this correct brother?"
 
 time.sleep(2)
-print "Reaching soundcloud.com/xxxxxxxxx"
+print "Reaching http://soundcloud.com/xxxxxxxxx "
 time.sleep(2)
 
 print "Waiting for link"
+time.sleep(2)
 print "Waiting for song"
+time.sleep(2)
 print "Song found"
+time.sleep(2)
 print "Downloading..."
+time.sleep(2)
 print "Downloading......."
 time.sleep(3)
 print "Download Complete"
-time.sleep(10)
-print "Thank you for using SoundCloudDownload by KURNlK"
+time.sleep(3)
+print "Thank you for using SoundCloudDownload by KURNlK please check your /downloads"
